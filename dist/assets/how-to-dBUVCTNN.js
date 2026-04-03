284528701df8f52c2fcc34603a2c3e9d2f873ad4
Theo Taylor <therrant@gmail.com>
2026-04-03 15:01:24 -0500

    Rebuild dist

--- dist/assets/how-to-dBUVCTNN.js
+++
@@ -1,1 +0,0 @@
-import"./supabase-client-BXEzLDpS.js";/* empty css                  */import{i}from"./common-CxfFcfZv.js";import"./messaging-5qyQ6ziq.js";import"./utils-C2W-HleY.js";i("howto",function(){});
