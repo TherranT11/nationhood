10bf03469a2fcd65df715265ad87aa18525ca11b
Theo Taylor <therrant@gmail.com>
2026-04-06 12:22:13 -0500

    Rebuild dist

---
+++ dist/assets/how-to-CXyS_YFB.js
@@ -0,0 +1,1 @@
+import"./supabase-client-BXEzLDpS.js";/* empty css                  */import{i}from"./common-Ool9bHq4.js";i("howto",function(){});
--- dist/assets/how-to-f5IaxoKv.js
+++
@@ -1,1 +0,0 @@
-import"./supabase-client-BXEzLDpS.js";/* empty css                  */import{i}from"./common-DC6iDOio.js";i("howto",function(){});
