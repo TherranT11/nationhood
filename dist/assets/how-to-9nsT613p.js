7474266b94c1c65fcd99d38ed26ba684061fa250
Theo Taylor <therrant@gmail.com>
2026-04-10 15:27:55 -0500

    Rebuild dist — Alpha 2.1.3.4

--- dist/assets/how-to-9nsT613p.js
+++
@@ -1,1 +0,0 @@
-import"./supabase-client-BXEzLDpS.js";/* empty css                  */import{i}from"./common-jXg-AZE1.js";i("howto",function(){});
---
+++ dist/assets/how-to-Civd7DJj.js
@@ -0,0 +1,1 @@
+import"./supabase-client-BXEzLDpS.js";/* empty css                  */import{i}from"./common-PsG67-8v.js";i("howto",function(){});
